3852782c6babb4db52de830af06a613577cb93fe
aj25687 <javantika.jain@gmail.com>
2026-02-28 09:17:16 -0500

    formatting....

--- logcleaning.sh
+++ logcleaning.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 
 date >> script_audit.log
-# echo Errors: >> script_audit.log
-mkdir processed_logs/ 
-mkdir quarantine/ 
+echo Errors: >> script_audit.log
+mkdir processed_logs/ 2>> script_audit.log 
+mkdir quarantine/ 2>> script_audit.log
+echo >> script_audit.log
 
 cd ./raw_logs #reletive path, change to absolute
 FILES= `find *.log`
